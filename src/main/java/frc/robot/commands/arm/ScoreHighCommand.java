478f51e0e3e8e7102e248c4fc7c035fe476f627e
Quentin Field <q@quentinfield.com>
2023-03-05 12:54:52 -0500

    Fixed ScoreHighCommand

--- src/main/java/frc/robot/commands/arm/ScoreHighCommand.java
+++ src/main/java/frc/robot/commands/arm/ScoreHighCommand.java
@@ -9,9 +9,9 @@ import frc.robot.subsystems.ArmSubsystem;
 public class ScoreHighCommand extends BaseArmCommand {
 
     private final ArmSubsystem armSubsystem;
-
-    // fixme: do everything - see table
-    // https://docs.google.com/document/d/1JzU-BzCXjGCwosouylmWGN83-x8lv-oPzklcXDqNN2U/edit#
+    private final ScoringRow   scoringRow;
+    private ArmPosition        scoringPosition = null;
+    private GamePiece          gamePiece       = null;
 
     public ScoreHighCommand(ArmSubsystem armSubsystem) {
         super(armSubsystem);
@@ -19,67 +19,43 @@ public class ScoreHighCommand extends BaseArmCommand {
         this.armSubsystem = armSubsystem;
 
         addRequirements(armSubsystem);
-
+        scoringRow = ScoringRow.TOP;
     }
 
     @Override
     public void initialize() {
 
+        gamePiece       = armSubsystem.getHeldGamePiece();
+        scoringPosition = ArmConstants.getScoringPosition(gamePiece, scoringRow);
+
         System.out.println("ScoreHighCommand started");
         printArmState();
         stopArmMotors();
-
     }
 
     @Override
     public void execute() {
 
-        // FIXME: What happens if we drop the piece while trying to position the arm?
-        GamePiece   gamePiece       = armSubsystem.getHeldGamePiece();
-
-        // FIXME: Do we need to call this every time or should it go into the initialize routine?
-        ArmPosition scoringPosition = ArmConstants.getScoringPosition(gamePiece, ScoringRow.TOP);
-
-        // ensure that we have a piece
-        // FIXME: If this is part of the isFinished() routine, it is not required here.
-        // How should this command finish?
-        // Maybe it finishes differently in auto than in teleop?
-        if (gamePiece == GamePiece.NONE) {
-            System.out.print("Can not score high, no piece is held.");
-        }
-
         // Move arm to scoring position
         boolean lift = moveArmLiftToAngle(scoringPosition.angle, ArmConstants.MAX_LIFT_SPEED);
         if (lift) {
-            boolean extend = moveArmExtendToEncoderCount(scoringPosition.extension, ArmConstants.MAX_EXTEND_SPEED);
-            if (extend) {
-                // FIXME: Do not print this message here, it should be in the isFinished routine.
-                System.out.print("ScoreHighCommand finished");
-            }
+            moveArmExtendToEncoderCount(scoringPosition.extension, ArmConstants.MAX_EXTEND_SPEED);
         }
-
-        // FIXME: Is this command responsible for the trimming? RECOMMENDED.
-        // Need to pass in the OperatorInput into this command in order to trim the height
-        // and extension of the arm.
     }
 
     @Override
     public boolean isFinished() {
 
-        // TELEOP:
-        // if (DriverStation.isTeleopEnabled())..
-        // FIXME: If this command does the trimming (recommended), then do not end this command
-        // It will be ended when it is interrupted by the ReleaseCommand.
-        // return false;
-
-        // AUTO:
-        // IF there is not a game piece, then the command should finish.
-        GamePiece   gamePiece       = armSubsystem.getHeldGamePiece();
-        ArmPosition scoringPosition = ArmConstants.getScoringPosition(gamePiece, ScoringRow.TOP);
+        // ensure that we have a piece and haven't dropped it
+        if (!armSubsystem.isGamePieceDetected()) {
+            System.out.print("Can not score high, no piece is held.");
+            return true;
+        }
 
         // Check position
         if (armSubsystem.isArmAtLiftAngle(scoringPosition.angle)
             && armSubsystem.isAtExtendPosition(scoringPosition.extension)) {
+            System.out.print("ScoreHighCommand finished");
             return true;
         }
         return false;
