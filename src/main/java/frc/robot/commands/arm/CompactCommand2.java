53c37f0dc8fac3151c8026d2b4a420838abaccab
Richard McMullin <richard.e.mcmullin@gmail.com>
2023-03-08 15:08:32 -0500

    Alternate StartIntake and PickupGamePiece

--- src/main/java/frc/robot/commands/arm/CompactCommand2.java
+++ src/main/java/frc/robot/commands/arm/CompactCommand2.java
@@ -23,50 +23,35 @@ public class CompactCommand2 extends BaseArmCommand {
     @Override
     public void execute() {
 
-        double  armAngle             = armSubsystem.getArmLiftAngle();
-        double  armExtensionPosition = armSubsystem.getArmExtendEncoder();
-
-        boolean armRetracted         = false;
-        boolean pincherInsideFrame   = false;
+        double armAngle             = armSubsystem.getArmLiftAngle();
+        double armExtensionPosition = armSubsystem.getArmExtendEncoder();
 
         /*
          * Special logic to make sure the arm comes up over the frame when retracting.
          */
-        if (armAngle < ArmConstants.CLEAR_FRAME_ARM_ANGLE) {
+        if (armAngle < ArmConstants.CLEAR_FRAME_ARM_ANGLE
+            && armExtensionPosition > ArmConstants.MAX_ARM_EXTEND_INSIDE_FRAME) {
 
-            // If the arm is inside the frame, then retract the arm and
-            // close the pincher to fit inside the frame.
-            if (armExtensionPosition < ArmConstants.MAX_ARM_EXTEND_INSIDE_FRAME) {
+            /*
+             * If the arm is not inside the frame, and the angle is below the clear frame angle,
+             * then raise the arm to clear the frame
+             */
 
-                armRetracted       = retractArm();
-                pincherInsideFrame = movePincherInsideFrame();
+            // Save time by closing the pincher at the same time
+            movePincherInsideFrame();
 
-                if (armRetracted && pincherInsideFrame) {
-                    lowerArmToBottom();
-                }
-            }
-            else {
-                /*
-                 * If the arm is not inside the frame, and the angle is below the clear frame angle,
-                 * then raise the arm to clear the frame
-                 */
-
-                // Save time by closing the pincher at the same time
-                movePincherInsideFrame();
-
-                // Ensure the arm is higher than the clear frame angle
-                moveArmLiftToAngle(ArmConstants.CLEAR_FRAME_ARM_ANGLE + ArmConstants.ARM_LIFT_ANGLE_TOLERANCE_DEGREES + 2);
-            }
+            // Ensure the arm is higher than the clear frame angle
+            moveArmLiftToAngle(ArmConstants.CLEAR_FRAME_ARM_ANGLE + ArmConstants.ARM_LIFT_ANGLE_TOLERANCE_DEGREES + 2);
 
             return;
         }
 
         /*
-         * The arm is now clearing the frame.
+         * The arm is now clearing the frame or is already retracted inside the frame
          */
 
-        armRetracted       = retractArm();
-        pincherInsideFrame = movePincherInsideFrame();
+        boolean armRetracted       = retractArm();
+        boolean pincherInsideFrame = movePincherInsideFrame();
 
         if (armRetracted && pincherInsideFrame) {
             lowerArmToBottom();
