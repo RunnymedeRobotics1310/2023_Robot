c39a55b7a325ad58262ba68d37ea9f1f8d9aaa19
Runnymede Driverstation <1310.runnymede.robotics@gmail.com>
2023-03-18 18:39:21 -0400

    Clean up imports

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,6 +1,13 @@
 package frc.robot.subsystems;
 
-import static frc.robot.Constants.VisionConstants.VisionTarget.*;
+import static frc.robot.Constants.VisionConstants.VisionTarget.APRILTAG_GRID;
+import static frc.robot.Constants.VisionConstants.VisionTarget.CONE_GROUND;
+import static frc.robot.Constants.VisionConstants.VisionTarget.CONE_SUBSTATION;
+import static frc.robot.Constants.VisionConstants.VisionTarget.CUBE_GROUND;
+import static frc.robot.Constants.VisionConstants.VisionTarget.CUBE_SUBSTATION;
+import static frc.robot.Constants.VisionConstants.VisionTarget.NONE;
+import static frc.robot.Constants.VisionConstants.VisionTarget.POST_HIGH;
+import static frc.robot.Constants.VisionConstants.VisionTarget.POST_LOW;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
