29f59987cb4fd61109355d150eb9160e1860987c
Tony Field <tony@kingsway.digital>
2023-03-22 18:32:56 -0400

    System.out.println for the robot init.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -26,6 +26,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotInit() {
+        System.out.println("Runnymede Robotics: Robot.robotInit()");
         // Instantiate our RobotContainer. This will perform all our button bindings,
         // and put our
         // autonomous chooser on the dashboard.
