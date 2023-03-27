7723aa8efc149a9da234b6b08672d7667e984f7a
Tony Field <tony@kingsway.digital>
2023-03-26 21:27:08 -0400

    Comment fix

--- src/main/java/frc/robot/commands/arm/StartIntakeCommand.java
+++ src/main/java/frc/robot/commands/arm/StartIntakeCommand.java
@@ -31,7 +31,7 @@ public class StartIntakeCommand extends BaseArmCommand {
         this.gamePiece       = gamePiece;
         this.operatorInput   = null;
         this.visionSubsystem = visionSubsystem;
-        // todo: fixme: add vision subsystem as requirement
+        // FIXME: add vision subsystem as requirement
     }
 
     public StartIntakeCommand(OperatorInput operatorInput, ArmSubsystem armSubsystem, VisionSubsystem visionSubsystem) {
@@ -44,7 +44,7 @@ public class StartIntakeCommand extends BaseArmCommand {
         this.visionSubsystem = visionSubsystem;
 
         this.gamePiece       = GamePiece.NONE;
-        // todo: fixme: add vision subsystem as requirement
+        // FIXME: add vision subsystem as requirement
     }
 
     @Override
