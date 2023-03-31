3fed9fb8244c0b130bd4688308a64234463f3a2b
Runnymede Driverstation <1310.runnymede.robotics@gmail.com>
2023-03-31 19:10:41 -0400

    Turn off logging

--- src/main/java/frc/robot/RunnymedeUtils.java
+++ src/main/java/frc/robot/RunnymedeUtils.java
@@ -25,6 +25,7 @@ public class RunnymedeUtils {
      * Note that the <code>distanceTravelled</code> must be specified, so calling
      * code needs to keep track of it (for example, by asking a motor
      * encoder what the distance travelled is).
+     * 
      * @param distanceToTravel The distance that should be travelled. This must be
      * a non-negative number
      * @param distanceTravelled The distance travelled so far. This value is used to
@@ -35,9 +36,9 @@ public class RunnymedeUtils {
      * <p>
      * It is the lowest speed that will be returned from this function at the beginning
      * of the acceleration period, and so should be suitable for being the next speed used
-     * by the system being controlled.  For example, if the previous speed is 0km/h
+     * by the system being controlled. For example, if the previous speed is 0km/h
      * and the <code>startSpeed</code> is 15km/h, the system should be able to safely
-     * handle jumping from 0 to 15.  The larger the gap from the current speed to the
+     * handle jumping from 0 to 15. The larger the gap from the current speed to the
      * <code>startSpeed</code>, the greater the stress on the system may be.
      * @param maxSpeed The target maximum speed to be reached during the cruise stage.
      * This number must be greater than startSpeed
@@ -55,13 +56,13 @@ public class RunnymedeUtils {
      * If the sum of <code>accelerationDistance</code> and <code>decelerationDistance</code>
      * exceeds the <code>distanceToTravel</code>, the acceleration will be proportionately
      * scaled down to ensure approximately the same acceleration curve.
-     * @return the fastest speed within the bounds specified.  If the <code>distanceTravelled</code>
+     * @return the fastest speed within the bounds specified. If the <code>distanceTravelled</code>
      * reaches or exceeds the <code>distanceToTravel</code>, a speed of 0 will be returned.
      * <p>
      * Note that if the <code>maxSpeed</code> is greater than 0, then when the
-     * <code>distanceToTravel</code> is reached, the next speed returned will be 0.  This
+     * <code>distanceToTravel</code> is reached, the next speed returned will be 0. This
      * could represent a discontinuity in speed by a using system if the returned value
-     * is not monitored.  For example, if a system should continue to coast once the
+     * is not monitored. For example, if a system should continue to coast once the
      * <code>distanceToTravel</code> is reached, the returned 0 value should be ignored.
      * @throws IllegalArgumentException if the input parameters are invalid
      */
@@ -69,14 +70,22 @@ public class RunnymedeUtils {
         final double startSpeed, double maxSpeed, final double endSpeed,
         double accelerationDistance, double decelerationDistance) throws IllegalArgumentException {
 
-        if (distanceToTravel < 0) throw new IllegalArgumentException("Distance to travel "+distanceToTravel+" must not be negative");
-        if (distanceTravelled < 0) throw new IllegalArgumentException("Distance travelled "+distanceTravelled+" must not be negative");
-        if (startSpeed < 0) throw new IllegalArgumentException("Start speed "+startSpeed+" must not be negative");
-        if (endSpeed < 0) throw new IllegalArgumentException("End speed "+endSpeed+" must not be negative");
-        if (maxSpeed < 0) throw new IllegalArgumentException("Max speed "+maxSpeed+" must not be negative");
-        if (maxSpeed < startSpeed) throw new IllegalArgumentException("Start speed "+startSpeed+" must not exceed max speed "+maxSpeed);
-        if (endSpeed > maxSpeed) throw new IllegalArgumentException("End speed "+endSpeed+" must not exceed max speed "+maxSpeed);
-        final boolean LOG_ENABLED = true;
+        if (distanceToTravel < 0)
+            throw new IllegalArgumentException("Distance to travel " + distanceToTravel + " must not be negative");
+        if (distanceTravelled < 0)
+            throw new IllegalArgumentException("Distance travelled " + distanceTravelled + " must not be negative");
+        if (startSpeed < 0)
+            throw new IllegalArgumentException("Start speed " + startSpeed + " must not be negative");
+        if (endSpeed < 0)
+            throw new IllegalArgumentException("End speed " + endSpeed + " must not be negative");
+        if (maxSpeed < 0)
+            throw new IllegalArgumentException("Max speed " + maxSpeed + " must not be negative");
+        if (maxSpeed < startSpeed)
+            throw new IllegalArgumentException("Start speed " + startSpeed + " must not exceed max speed " + maxSpeed);
+        if (endSpeed > maxSpeed)
+            throw new IllegalArgumentException("End speed " + endSpeed + " must not exceed max speed " + maxSpeed);
+        final boolean LOG_ENABLED = false;
+        // final boolean LOG_ENABLED = true;
 
         if (distanceTravelled >= distanceToTravel) {
             return 0;
@@ -86,35 +95,39 @@ public class RunnymedeUtils {
         double ratio = distanceToTravel / (accelerationDistance + decelerationDistance);
         if (ratio < 1) {
 
-            maxSpeed = maxSpeed * ratio;
+            maxSpeed             = maxSpeed * ratio;
             accelerationDistance = accelerationDistance * ratio;
             decelerationDistance = decelerationDistance * ratio;
 
             if (LOG_ENABLED) {
-                System.out.println("Short distance - reducing max speed to "+maxSpeed);
+                System.out.println("Short distance - reducing max speed to " + maxSpeed);
             }
         }
 
-        double remaining = distanceToTravel - distanceTravelled;
+        double       remaining = distanceToTravel - distanceTravelled;
         final double speed;
 
         if (distanceTravelled < accelerationDistance) {
 
             // speeding up
-            double pctDriven = (distanceTravelled/accelerationDistance);
-            speed = Math.max(startSpeed, pctDriven*maxSpeed);
+            double pctDriven = (distanceTravelled / accelerationDistance);
+            speed = Math.max(startSpeed, pctDriven * maxSpeed);
             if (LOG_ENABLED) {
-                System.out.println("Accelerating ("+Math.round(pctDriven*100)+"% done). Speed: "+(Math.round(speed*100)/100d)+" dist/total: "+(Math.round(distanceTravelled))+"/"+distanceToTravel);
+                System.out.println(
+                    "Accelerating (" + Math.round(pctDriven * 100) + "% done). Speed: " + (Math.round(speed * 100) / 100d)
+                        + " dist/total: " + (Math.round(distanceTravelled)) + "/" + distanceToTravel);
             }
 
         }
         else if (remaining < decelerationDistance) {
 
             // slowing down
-            double pctToGo = (remaining/decelerationDistance);
-            speed = Math.max(endSpeed, pctToGo*maxSpeed);
+            double pctToGo = (remaining / decelerationDistance);
+            speed = Math.max(endSpeed, pctToGo * maxSpeed);
             if (LOG_ENABLED) {
-                System.out.println("Decelerating ("+Math.round(pctToGo*100)+"% done). Speed: "+(Math.round(speed*100)/100d)+" dist/total: "+(Math.round(distanceTravelled))+"/"+distanceToTravel);
+                System.out
+                    .println("Decelerating (" + Math.round(pctToGo * 100) + "% done). Speed: " + (Math.round(speed * 100) / 100d)
+                        + " dist/total: " + (Math.round(distanceTravelled)) + "/" + distanceToTravel);
             }
 
         }
